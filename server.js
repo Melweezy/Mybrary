530df42af61488c0284a963406de55c630538393
Melvin Schenker <melvin-schenker@hotmail.com>
2021-03-20 16:48:45 +0100

    added models

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-if (process.env.NODE_ENV !== 'production') {
+//load variables from .env-file, import into process.env
+if (process.env.NODE_ENV != 'production') {
 require('dotenv').config()
 }
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 //import routes
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 //folders
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+//import mongoose
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
-db.on('error', error => console.error(error))//print error 
-db.once('open', () => console.log('Connected to Mongoose'))//print first status 
+db.on('error', error => console.error(error)) //print error
+db.once('open', () => console.log('Connected to Mongoose')) //print first connect
 //indexRouter handels '/'-route
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 //listen
 app.listen(process.env.PORT || 3000)
